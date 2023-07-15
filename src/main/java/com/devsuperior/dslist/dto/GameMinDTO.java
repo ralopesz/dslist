2032d9b60cde33a81dfdbea73da3a469b33755f1
Ricardo Lopes <ralopesz@gmail.com>
2023-07-15 17:43:35 -0300

    Game repository, service, controller

---
+++ src/main/java/com/devsuperior/dslist/dto/GameMinDTO.java
@@ -0,0 +1,46 @@
+package com.devsuperior.dslist.dto;
+
+import com.devsuperior.dslist.entities.Game;
+
+public class GameMinDTO {
+
+    private Long id;
+    private String title;
+    private Integer year;
+    private String imgUrl;
+    private String shortDescription;
+
+    public GameMinDTO(){}
+
+    public GameMinDTO(Game entity) {
+        id = entity.getId();
+        title = entity.getTitle();
+        year = entity.getYear();
+        imgUrl = entity.getImgUrl();
+        shortDescription = entity.getShortDescription();
+    }
+
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public String getTitle() {
+        return this.title;
+    }
+
+    public Integer getYear() {
+        return this.year;
+    }
+
+    public String getImgUrl() {
+        return this.imgUrl;
+    }
+
+    public String getShortDescription() {
+        return this.shortDescription;
+    }
+   
+
+
+}
